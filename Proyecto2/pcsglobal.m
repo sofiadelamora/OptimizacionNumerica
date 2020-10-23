752b32cba8d8a4270de16db81a22c5d6bd108440
sofiadelamora <sofiadlm3001@gmail.com>
2020-10-23 12:29:52 -0500

    global update

--- Proyecto2/pcsglobal.m
+++ Proyecto2/pcsglobal.m
@@ -1,21 +1,36 @@
 function [x, lambda, k] = pcsglobal(fx, hx, x0)
-% M´etodo de programaci´on Cuadr´atica Sucesiva con b´usqueda de lineal,
-% usando la funci´on de m´erito L-1 y actualizaci´on de la hessiana
-% con la f´ormula BF GS para el problema
+% Metodo de programacion Cuadratica Sucesiva con busqueda de lineal,
+% usando la funcion de merito L-1 y actualizacion de la hessiana
+% con la formula BF GS para el problema
 % Min fx
 % Sujeto a hx = 0
 %
 % fx y hx son cadenas de caracteres con las funciones en Matlab
-% de la funci´on objetivo y las restricciones del problema
+% de la funcion objetivo y las restricciones del problema
 % El vector x0 es el valor inicial
 % Salida
-% x.- aproximaci´on al m´?nimo local
-% ?.- multiplicador de Lagrange asociado a x.
-% k.- n´umero de iteraciones realizadas.
+% x.- aproximacion al minimo local
+% lambda.- multiplicador de Lagrange asociado a x.
+% k.- numero de iteraciones realizadas.
 %
 % Debe usar las funciones: gradiente.m y jacobiana.m para calcular
 % las primeras derivadas.
-%
-% Nombres completos de los integrantes del equipo
-% Instrucciones del programa documentados.
+%-----------------------------------------------------
+%Luis Guillermo Pizana
+%Sofia De la Mora 
+%Optimizacion Numerica
+%Proyecto 2
+%05 noviembre 2020
+%------------------------------------------------------
+%Parametros iniciales
+%n
+%m
+tol=10e^-05;
+maxk=100;
+c_1=10e^-02;
+c_0=1;
+%x_0
+lambda=zeros(m,1);
+B_0= eye(n);
+k=0;
 end
